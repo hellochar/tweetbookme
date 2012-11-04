51f2b795d6cd7aef76cbce93bcac3dad90b2e427
Xiaohan Zhang <hellocharlien@hotmail.com>
2012-11-03 17:52:13 -0700

    shit's working

--- tweetbookme.py
+++ tweetbookme.py
@@ -1,24 +1,75 @@
-import bottle
 from bottle import route, run
-from facebook import *
+import facebook
+import pprint
+from time import gmtime, strftime
+import urllib2
+import json
+import urllib
+import itertools
 
-APP_KEY = ""
-APP_SECRET = ""
+APP_ID = "125942107559245"
+APP_SECRET = "b662899fe149bd0e552fd9a1f12b157f"
 
 HOST = ""
 TOKEN_STORE = {}
 cookies = {}
+ACCESS_TOKEN = "AAACEdEose0cBAONOmWh7pSwGUQXA05GwABy4nlhiwJi6XSG9YOFr7L9xaJUVZCmZBMB0Ou5kWVB4kfJcRW0jNaEA74bn2YuZAhegF2s5gZDZD"
+
+
+def flatten(l):
+    return list(itertools.chain(*l))
 
 @route('/')
 def index_page():
-    sess = get_session()
-    request_token = sess.obtain_request_token()
-    TOKEN_STORE[request_token.key] = request_token
-    
-    callback = "http://%s/callback/" % (bottle.request.headers['host'])
-    url = sess.build_authorize_url(request_token, oauth_callback=callback)
-    prompt = """Click <a href="%s">here</a> to link with Dropbox."""
-    return prompt % url 
+    graph = facebook.GraphAPI(ACCESS_TOKEN)
+    profile = graph.get_object("me")
+    likes = graph.get_connections("me", "likes") 
+    formatted = pprint.pformat(likes)
+
+    names_array = [like["name"] for like in likes["data"]]
+
+    def try_encode(name):
+        try:
+            return ["http://search.twitter.com/search.json?" + urllib.urlencode({"q": name, "result_type": "popular"})]
+        except:
+            return []
+
+    urls_array = flatten([try_encode(name) for name in names_array])
+    tweet_responses = []
+    for url in urls_array:
+        try:
+            response = urllib2.urlopen(url)
+            tweet_responses.append(json.load(response))
+            print "correct " + url
+        except urllib2.HTTPError, e:
+            print "skipping " + url
+            pass
+
+    all_tweets = flatten([tweet['results'] for tweet in tweet_responses])
+    useful_array = [(t["metadata"]["recent_retweets"], t["text"]) for t in all_tweets]
+    sorted_tweets = sorted(useful_array, key=lambda x: x[0])
+
+    graph.put_wall_post("test message:" + sorted_tweets[0][1])
+
+    return sorted_tweets.join('<br>')
+
+@route('/callback/')
+def callback_page():
+    # note: the OAuth spec calls it 'oauth_token', but it's
+    # actually just a request_token_key...
+    request_token_key = bottle.request.params['code']
+    if not request_token_key:
+        return "Expected a request token key back!"
+
+    request_token = TOKEN_STORE[request_token_key]
+    access_token = sess.obtain_access_token(request_token)
+    TOKEN_STORE[access_token.key] = access_token
+    cookies['access_token_key'] = access_token.key
+    return bottle.redirect('/viewfiles/')
+  
+@route('/complete_link/')
+def complete():
+  return 'Link complete'
 
 def get_session():
     return session.FacebookSession(APP_KEY, APP_SECRET, ACCESS_TYPE)
@@ -27,3 +78,5 @@ def get_client(access_token):
     sess = get_session()
     sess.set_token(access_token.key, access_token.secret)
     return client.DropboxClient(sess)
+
+run(host='localhost', port=80, debug=True)
